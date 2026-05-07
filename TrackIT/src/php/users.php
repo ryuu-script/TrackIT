4fa7d38076c819627991eb3930734c548306c73d
Ryuu‪ ♥︎ <christian296112@gmail.com>
2026-05-08 01:23:34 +0800

    Added all starting files

---
+++ TrackIT/src/php/users.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once '../php/component/db_connect.php'; 
+require_once '../php/component/navbar.php';
+
+// Start coding from here
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="/TrackIT/src/css/navbar.css">
+
+    <title>TrackIT | Users</title>
+
+    <!-- Start coding from here -->
+</head>
+<body>
+    <!-- Start coding from here -->
+    
+</body>
+</html>
